e1166de41d1d8a9f13001493d3b084fea27ffbee
ilya0 <iosovets@gmail.com>
2017-06-26 12:34:42 -0700

    added hyperlink

--- src/bookmarks.ts
+++ src/bookmarks.ts
@@ -4,19 +4,23 @@ export class App {
     heading = "Links Saver";
     links: Link[] = [];
     linkDescription = '';
+    linkDescription2 = '';
 
 
     //adds the
     addLink() {
+
         if (this.linkDescription) {
             console.log("add todo ran")
             // this.links.push(new Todo(this.linkDescription));
 
             var item = new Link();
             item.description = this.linkDescription;
+            item.description2 = this.linkDescription2
             this.links.push(item);
 
             this.linkDescription = '';
+            this.linkDescription2 = '';
         }
         console.log(this.links);
     }
--- src/link.ts
+++ src/link.ts
@@ -1,10 +1,12 @@
-// create a class to hold the todo object
+// create a class to hold the link object
 
 export class Link {
     done: boolean = false;
     description: string;
+    description2: string;
 
-    constructor(description?: string) {
+    constructor(description?: string, description2?: string) {
         this.description = description;
+        this.description2 = description;
     }
 }
